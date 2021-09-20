60a39f14368e9a6ae24b0f26456888c66d577e77
Advik-B <advik.b@gmail.com>
2021-09-20 15:33:41 +0530

    😀

---
+++ trans.py
@@ -0,0 +1,69 @@
+import googletrans
+
+_available_lang = googletrans.LANGUAGES.items()
+
+langs = dict()
+langs2 = dict()
+
+ON = True
+OFF = False
+
+class TranslateError(Exception):
+    pass
+class AutoDectectError(Exception):
+    pass
+
+for lang_prefix , lang in _available_lang:
+    langs.update({str(lang_prefix): str(lang)})
+    langs2.update({str(lang) : str(lang_prefix)})
+
+trans = googletrans.Translator()
+
+def convertLang(
+    text:str,
+
+    output_lang:str='en',
+
+    auto_dectect:bool=ON,
+
+    source:str=None,
+
+    ):
+    """Will translate the text in given language
+    -"""
+    if source == None:
+        _source = 'en'
+    else:
+        if source in langs2.keys():
+            __out = langs2[source]
+        elif source in langs.keys():
+            __out = langs[source]
+        else:
+            raise TranslateError(f'No language match with `{output_lang}`')
+        _source = source
+
+    if auto_dectect == OFF:
+
+        if output_lang in langs2.keys():
+            __out = langs2[output_lang]
+        elif output_lang in langs.keys():
+            __out = langs[output_lang]
+        else:
+            raise TranslateError(f'No language match with `{output_lang}`')
+
+        _out = trans.translate(text , src = _source , dest=output_lang)
+        return _out.text
+
+    elif auto_dectect == ON:
+        if output_lang in langs2.keys():
+            __out = langs2[output_lang]
+        elif output_lang in langs.keys():
+            __out = langs[output_lang]
+        else:
+            raise TranslateError(f'No language match with `{output_lang}`')
+
+        _out = trans.translate(text , dest=output_lang)
+        return _out.text
+    
+    else:
+        raise AutoDectectError(f'Unknown Auto-Dectect mode :{auto_dectect}')
